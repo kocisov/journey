31553ecd6a73e9ad4e4c16879ca6ee376d81683a
kocisov <kocis.34@gmail.com>
2018-10-12 20:14:59 +0200

    typescript and rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'bin/journey.bundle.js',
+    format: 'cjs',
+  },
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+  ],
+}
